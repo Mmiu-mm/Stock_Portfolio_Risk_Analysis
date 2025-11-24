160ecd605ec0987dbbac6b0911be9dbc2032d792
mm <1020161803@qq.com>
2025-11-24 09:35:05 +0800

    modify

--- sections/sidebar.py
+++ sections/sidebar.py
@@ -3,7 +3,7 @@ import streamlit as st
 def render_sidebar():
     with st.sidebar:
         try:
-            st.image(r"D:\final_project\logo\efrei.png", caption="EFREI", width=180, use_container_width=False)
+            st.image(r"D:\final_project\logo\efrei.png", caption="EFREI", width=150, use_container_width=False)
         except Exception as e:
             st.error(f"Could not load EFREI Logo: {e}")
             st.markdown("**EFREI Logo**")
@@ -11,24 +11,22 @@ def render_sidebar():
         st.write("")
             
         try:
-            st.image(r"D:\final_project\logo\WUT.png", caption="WUT", width=180, use_container_width=False)
+            st.image(r"D:\final_project\logo\WUT.png", caption="WUT", width=150, use_container_width=False)
         except Exception as e:
             st.error(f"Could not load WUT Logo: {e}")
-            st.markdown("**WUT Logo**")
-        
+            st.markdown("**WUT Logo**")  
         st.markdown("---")
-
-        st.title("Portfolio Risk Analysis")
-    
+        st.title("Course: Data Visualization 2025-Portfolio Risk Analysis")    
         st.markdown("---")
         st.subheader("Student Info")
         
-        st.write("**Supervisor:** Mano Joseph Mathew")
+        st.write("**Supervisor:** Prof. Mano Mathew")
         st.write("**Email:** mano.mathew@efrei.fr")
-
+        st.markdown("[Check out this LinkedIn](https://www.linkedin.com/in/manomathew/)", unsafe_allow_html=True)
         st.write("**Student Name:** Yalin Mo")
         st.write("**Email:** Yalin.Mo@efrei.net")
-       
+        st.markdown("[Check out this GitHub](https://github.com/Mmiu-mm/Stock_Portfolio_Risk_Analysis.git)", unsafe_allow_html=True)
+    
         st.markdown("---")
         
         st.subheader("Analysis Modules")
@@ -39,7 +37,5 @@ def render_sidebar():
             index=0
         )
         
-        st.markdown("---")
-        st.markdown("https://github.com/Mmiu-mm/Stock_Portfolio_Risk_Analysis.git")
     
     return selected_module
